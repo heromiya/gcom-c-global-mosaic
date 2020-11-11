de4b6b939ccc682658d8808eab1049037e6b91ed
Hiroyuki Miyazaki <heromiya@heromiya.net>
2020-11-11 11:49:02 +0000

    new file:   composite.R 	new file:   getRSRF.sh 	new file:   h5_2_tiff.py

---
+++ composite.R
@@ -0,0 +1,57 @@
+## install.packages("rgdal")
+## install.packages("raster")
+library("rgdal")
+library("raster")
+
+args = commandArgs(trailingOnly=TRUE)
+
+R <- brick(args[1])
+G <- brick(args[2])
+B <- brick(args[3])
+N <- brick(args[4])
+
+outfile <- args[5]
+
+ysize <- dim(R)[1]
+xsize <- dim(R)[2]
+ndays <- dim(R)[3]
+
+rotate <- function(x) apply(t(x),2,rev)
+flip <- function(m) m[c(nrow(m):1),]
+
+input <- array(c(values(B)
+                ,values(G)
+                ,values(R)
+                ,values(N))
+              ,dim=c(xsize,ysize,ndays,4))
+
+composite <- function(x){
+    ndvi = (x[,4] - x[,3]) / (x[,4] + x[,3])
+    idx = match(quantile(ndvi,probs=seq(0.9,1.0,0.1),type=3,na.rm=TRUE)[1],ndvi)
+    R = x[idx,3]
+    G = x[idx,2]
+    B = x[idx,1]
+    c(R,G,B)
+}
+
+out <- apply(input,c(1,2),composite)
+#out <- array(c(out[1,,],out[2,,],out[3,,]),dim=c(ysize,xsize,3))
+
+Rout <- raster(ncol=xsize, nrow=ysize, crs=NA)
+extent(Rout) <- extent(R)
+values(Rout) <- flip(rotate(out[3,,]))
+
+Gout <- raster(ncol=xsize, nrow=ysize, crs=NA)
+extent(Gout) <- extent(G)
+values(Gout) <- flip(rotate(out[2,,]))
+
+Bout <- raster(ncol=xsize, nrow=ysize, crs=NA)
+extent(Bout) <- extent(B)
+values(Bout) <- flip(rotate(out[1,,]))
+
+StackOut <- stack(Rout,Gout,Bout)
+
+#writeRaster(StackOut,filename="out.tif",overwrite=TRUE,options="compress=deflate")
+writeRaster(StackOut,filename=outfile
+           ,options="COMPRESS=Deflate"
+           ,overwrite=TRUE)
---
+++ getRSRF.sh
@@ -0,0 +1,47 @@
+#! /bin/bash
+
+export TILE=0529
+export GCP_INTERVAL=100
+export RES=250
+#export YEAR=2019
+export WARPOPT="-q -s_srs EPSG:4326 -t_srs EPSG:4087 -tr $RES $RES -tps -r lanczos -multi -co COMPRESS=Deflate -overwrite"
+
+mkdir -p GCOM-C/$TILE
+mkdir -p VRT
+
+getRSRF() {
+    WORKDIR=$(mktemp -d)
+    DOY=$1
+    DATE_STRING="jan 1 2020 $DOY days"
+    export YYYY=$(date --date="$DATE_STRING" +%Y)
+    export MM=$(date --date="$DATE_STRING" +%m)
+    export DD=$(date --date="$DATE_STRING" +%d)
+
+    H5FILE=$WORKDIR/GC1SG1_${YYYY}${MM}${DD}D01D_T${TILE}_L2SG_RSRFQ_2000.h5
+    #H5FILE=$WORKDIR/GC1SG1_${YYYY}${MM}${DD}D01D_T${TILE}_L2SG_LTOAK_1002.h5
+    FTP=ftp://ftp.gportal.jaxa.jp/standard/GCOM-C/GCOM-C.SGLI/L2.LAND.RSRF
+    #FTP="ftp://ftp.gportal.jaxa.jp/standard/GCOM-C/GCOM-C.SGLI/L2.LAND.LTOA"
+    
+    wget -q -nc --user=heromiya --password=anonymous $FTP/2/$YYYY/$MM/$DD/$(basename $H5FILE) -O $H5FILE
+    for B in VN04 VN06 VN07 VN10; do # VN03 VN05 VN07
+      	python3 h5_2_tiff.py $H5FILE Rs_${B} $H5FILE.$B.tif $GCP_INTERVAL
+	gdalwarp $WARPOPT $H5FILE.$B.tif GCOM-C/$TILE/$(basename $H5FILE).$B.tif
+    done
+
+    # Not stable to use resampled products.
+    #    VGI_H5FILE=GC1SG1_${YYYY}${MM}${DD}D01D_T${TILE}_L2SG_VGI_Q_2000.h5
+    #    wget -nc --user=heromiya --password=anonymous ftp://ftp.gportal.jaxa.jp/standard/GCOM-C/GCOM-C.SGLI/L2.LAND.VGI_/2/$YYYY/$MM/$DD/$VGI_H5FILE -O GCOM-C/$VGI_H5FILE
+    #    python3 h5_2_tiff.py GCOM-C/$VGI_H5FILE EVI $WORKDIR/$VGI_H5FILE.EVI.tif $GCP_INTERVAL
+    #    gdalwarp $WARPOPT $WORKDIR/$VGI_H5FILE.EVI.tif GCOM-C/$VGI_H5FILE.EVI.tif
+    rm -rf $WORKDIR
+}
+export -f getRSRF
+
+#parallel getRSRF ::: {366..730}
+parallel getRSRF ::: {0..314}
+#for i in {268..273}; do getRSRF $i; done
+for B in VN04 VN06 VN07 VN10; do
+    gdalbuildvrt -separate -overwrite VRT/$TILE.$B.vrt $(pwd)/GCOM-C/$TILE/*T${TILE}*RSRF*.$B.tif
+done
+
+Rscript composite.R VRT/$TILE.VN04.vrt VRT/$TILE.VN06.vrt VRT/$TILE.VN07.vrt VRT/$TILE.VN10.vrt out.v2.100.250.tif
