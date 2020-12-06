20fb4e2ceb0e9d8011ec56c37377e2b4d59c7851
heromiya <heromiya@heromiya.net>
2020-12-06 11:57:22 +0900

    modified:   batch.qsub_exec.sh 	modified:   composite.R 	modified:   getRSRF.sh 	modified:   qsub_exec.sh

--- batch.qsub_exec.sh
+++ batch.qsub_exec.sh
@@ -1,26 +1,20 @@
 #! /bin/bash
 
-#for TILE in $(sort tile.lst | uniq | grep -v 0528 | grep -v 0529); do #0528 0529
-
-#for TILE in  1617 0313 0619 1130; do #0528 0529 
+#for TILE in  1617  1130; do #
 #    qsub -g tgh-20IAV qsub_exec.sh $TILE
 #done
 
 
-#for HH in {0..35}; do
-#    for VV in {0..17}; do
-#	TILE=$(printf %02d%02d $VV $HH)
-#	qsub -g tgh-20IAV qsub_exec.sh $TILE
-#    done
-#done
-#if [ "$(grep $TILE RSRF.completed.lst)" = "" ]; then  ; fi
-for TILE in 0113; do # 0117 0214 1513 1615 1618
-     qsub -g tgh-20IAV qsub_exec.sh $TILE
+for HH in {0..35}; do
+    for VV in {0..17}; do
+	TILE=$(printf %02d%02d $VV $HH)
+	echo "0619 0528 0529 0113 0117 0214 1513 1615 1618 0313" | grep $TILE
+	if [ $? -ne 0 ]; then
+	    qsub -g tgh-20IAV qsub_exec.sh $TILE
+	fi
+    done
 done
-
-#for PATH in 1..485; do
-    
-#    qsub -g tgh-20IAV qsub_exec.sh $TILE
+#if [ "$(grep $TILE RSRF.completed.lst)" = "" ]; then  ; fi
+#for TILE in 0619 0528 0529 0113 0117 0214 1513 1615 1618 0313; do
+#     qsub -g tgh-20IAV qsub_exec.sh $TILE
 #done
-
-#qsub -g tgh-20IAV qsub_exec.NWLR.sh 03010
